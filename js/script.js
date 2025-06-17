7349f47e6bae9871529de07e3a0ded1c7821b077
Siti Arbaisyah <sitiarbaisyah0106@gmail.com>
2025-06-17 19:04:11 +0700

    form pencarian

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-// Toggle classs active
+// Toggle classs active untuk hamburger
 const navbarNav = document.querySelector(".navbar-nav");
 
 // ketika hamburger menu di klik
@@ -6,11 +6,29 @@ document.querySelector("#hamburger-menu").onclick = () => {
   navbarNav.classList.toggle("active");
 };
 
-// klik di luar sidebar untuk menghilangkan nav
-const hamburger = document.querySelector("#hamburger-menu");
+// Toggle classs active untuk seacrh form
+const searchForm = document.querySelector('.search-form');
+const searchBox = document.querySelector('#search-box');
 
-document.addEventListener("click", function (e) {
-  if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
-    navbarNav.classList.remove("active");
+document.querySelector('#search-button').onclick = (e) => {
+ searchForm.classList.toggle('active');
+ searchBox.focus();
+ e.preventDefault();
+
+};
+
+
+// klik di luar elemen
+const hm = document.querySelector("#hamburger-menu");
+const sb = document.querySelector('#search-button');
+
+document.addEventListener('click', function (e) {
+  if (!hm.contains(e.target) && !navbarNav.contains(e.target)) {
+    navbarNav.classList.remove('active');
   }
+
+  if (!sb.contains(e.target) && !searchForm.contains(e.target)) {
+    searchForm.classList.remove('active');
+  }
+
 });
